eb20518c7f5d6d2b9b65a69cb81834f2bc69e473
lukasberglund <lukastberglund@gmail.com>
2022-08-03 14:25:22 -0700

    change default

--- mbag/environment/goals/transforms.py
+++ mbag/environment/goals/transforms.py
@@ -59,7 +59,7 @@ class AddGrassTransform(GoalTransform):
     blocks.
     """
 
-    default_config: AddGrassTransformConfig = {"mode": "surround"}
+    default_config: AddGrassTransformConfig = {"mode": "replace"}
     config: AddGrassTransformConfig
 
     def generate_goal(self, size: WorldSize) -> MinecraftBlocks:
